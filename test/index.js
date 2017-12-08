d958ef1a35ce24f5cbee821661e2857433e13ff1
Luke Edwards <luke.edwards05@gmail.com>
2017-12-07 18:37:32 -0800

    add tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,10 @@ const test = require('tape');
 const fn = require('../dist/is-offline');
 
 test('is-offline', t => {
-	t.is(typeof fn, 'function', 'exports a function');
+	t.is(typeof fn, 'object', 'exports an object');
+	let keys = Object.keys(fn);
+	t.is(keys.length, 2, 'exports two methods');
+	t.is(typeof fn.watch, 'function', `exports.watch is a function`);
+	t.is(typeof fn.default, 'function', `exports.default is a function`);
 	t.end();
 });
